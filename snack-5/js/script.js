14241ab3c5abe63398f446849234da05a33884d0
manfred-commits <manfredipiraino@gmail.com>
2021-08-25 15:57:38 +0200

    aggiunta richiesta a video di 6 numeri

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,2 +1,7 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
-alert("hello, Peter");
+// 1 creare un array vuoto
+var contenitore = [];
+for(var i=0;i<6;i++){
+    // 2) Chiedi per 6 volte all’utente di inserire un numero
+    var inputUtente= parseInt(prompt("Inserisci il " + (i+1) +"° numero:"));
+}
