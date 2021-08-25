19732bedf9ce96af7c6fc82e8ec23025967b3526
manfred-commits <manfredipiraino@gmail.com>
2021-08-25 16:35:51 +0200

    aggiunta fix: array push

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -4,6 +4,7 @@ var contenitore = [];
 for(var i=0;i<6;i++){
     // 2) Chiedi per 6 volte all’utente di inserire un numero
     var inputUtente= parseInt(prompt("Inserisci il " + (i+1) +"° numero:"));
+    // 3) Inserisci una verifica input utente, per assicurarsi che venga inserito un numero
     if(isNaN(inputUtente)==true){
         alert("Inserisci un numero!!");
         i--;
@@ -11,10 +12,9 @@ for(var i=0;i<6;i++){
     else if(isNaN(inputUtente)==false){
         // condizione per il caso dispari
         if(inputUtente%2==1){
-            contenitore[i]=inputUtente;
+            contenitore.push(inputUtente);
         }
-    }    
-    
+    }     
 }
 for(var i=0;i<contenitore.length;i++){
     alert("Il contenuto dell'array in posizione "+ (i+1) + " è : "+contenitore[i]);
